bbb78b25b5f8869d9110c251a36cf0cd8f28f5e3
rodri <rodri>
2026-08-15 12:48:54 -0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,12 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/',
+
+  base: '/minesweeper/',
 })
